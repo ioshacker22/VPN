6c2a70cd00221cbfa69d532548a15dd7e63c3820
David Adebambo <its_adebambo@yahoo.com>
2025-12-27 08:33:04 -0500

    changes

---
+++ Build/CMakeFiles/VPN.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VPN.dir/SRC/main.cpp.o"
+  "CMakeFiles/VPN.dir/SRC/main.cpp.o.d"
+  "VPN"
+  "VPN.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VPN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
