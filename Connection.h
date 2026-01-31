0a0b686a4a56e39d3a54143c389f5f135a085003
David Adebambo <its_adebambo@yahoo.com>
2026-01-31 13:16:19 -0500

    connection.h

---
+++ Connection.h
@@ -0,0 +1,24 @@
+#ifndef SENECA_CONNECTION_H
+#define SENECA_CONNECTION_H
+#include <string>
+
+namespace seneca{
+
+
+class Connection{
+
+    public: 
+    virtual void connect() =0;
+    virtual void authenticate() = 0;
+    virtual void sendData(const std::string& ) = 0;
+    virtual void receiveData() = 0;
+    virtual void disconnect() = 0;
+    virtual ~Connection() = 0;
+    virtual Connection* clone() = 0;
+
+};
+Connection::~Connection(){};
+
+}
+
+#endif
